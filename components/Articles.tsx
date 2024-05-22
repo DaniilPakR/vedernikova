8c48fb9a8232c4b3b662e726f38b6f66d3abf236
Daniel Park <sanva970@gmail.com>
2024-05-22 20:29:32 +0300

    desktop styles

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -11,15 +11,15 @@ export default function Articles() {
                 <div className="articles-column flex flex-col items-center text-center">
                     <div className="articles-item-1 flex flex-col">
                         <Image src="/article-pictures/article-1.webp" height={151} width={202} alt="article-img-1" className="article-img-1 justify-self-center m-auto" />
-                        <h1 className="article-text font-semibold">
+                        <h1 className="article-text article-text-1 font-semibold">
                             “Как создать и оформить<br />лид-магнит”
                         </h1>
                     </div>
                     <Image src="/small-pictures/svg-gobbler.svg" width={24} height={24} alt="start-picture" />
                     <div className="articles-item-2 flex flex-col">
                         <Image src="/article-pictures/article-2.webp" height={188} width={172} alt="article-img-2" className="article-img-2 justify-self-center m-auto" />
-                        <h1 className="article-text font-semibold">
-                            “Разбор воронки по шагам, которая принесла мне 1.5 млн. рублей за 3 месяца”
+                        <h1 className="article-text article-text-2 font-semibold">
+                            “Разбор воронки по шагам, которая<br className="break-line-desktop hidden" /> принесла мне 1.5 млн. рублей за 3 месяца”
                         </h1>
                     </div>
                 </div>
