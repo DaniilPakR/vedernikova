047bdeb2f13f27b5c1c9e55e5d7e754893d432fb
ibrvimv <ibrvimv@gmail.com>
2024-05-26 18:53:11 +0500

    test Adlet

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,42 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import ForWho from "@/components/ForWho";
-import PracticeLesson from "@/components/PracticeLesson";
-import Price from "@/components/Price";
-import Articles from "@/components/Articles";
-import Faq from "@/components/Faq";
-import Indicators from "@/components/Indicators";
-import Ending from "@/components/Ending";
-import Footer from "@/components/Footer";
-import Doubts from "@/components/Doubts";
-
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
+import ForWho from '@/components/ForWho';
+import PracticeLesson from '@/components/PracticeLesson';
+import Price from '@/components/Price';
+import Articles from '@/components/Articles';
+import Faq from '@/components/Faq';
+import Indicators from '@/components/Indicators';
+import Ending from '@/components/Ending';
+import Footer from '@/components/Footer';
+import Doubts from '@/components/Doubts';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className='flex flex-col'>
+      ADLET TEST
       <Header />
       <Hero />
-      <div id="about">
+      <div id='about'>
         <About />
       </div>
-      <div id="for-who">
+      <div id='for-who'>
         <ForWho />
       </div>
-      <div id="practice-lesson">
+      <div id='practice-lesson'>
         <PracticeLesson />
       </div>
-        <Doubts />
-      <div id="price">
+      <Doubts />
+      <div id='price'>
         <Price />
       </div>
       <Articles />
-      <div id="faq">
+      <div id='faq'>
         <Faq />
       </div>
       <Indicators />
       <Ending />
-      <div id="footer">
+      <div id='footer'>
         <Footer />
       </div>
     </div>
