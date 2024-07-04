1ed81af0b7948d6f7fb977db52b76ea3c7dc17b9
Daniel Park <sanva970@gmail.com>
2024-07-04 12:39:26 +0500

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
@@ -12,58 +11,13 @@ import Ending from '@/components/Ending';
 import Footer from '@/components/Footer';
 import Doubts from '@/components/Doubts';
 import Reviews from '@/components/Reviews';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-type DataType = {
-  id: number;
-  name: string;
-}
 
 export default function Home() {
-  const [data, setData] = useState([])
-
-  const fetchData = async () => {
-
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-      console.log('fetched')
-      setData(response.data);
-      console.log(data)
-    } catch (err) {
-      console.error(err)
-    }
-  };
-
-
-
-  const handleDelete = async (id: number) => {
-    try {
-      const response = await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
-      console.log(response)
-      fetchData()
-    } catch (err) {
-      console.error(err)
-    }
-  };
-
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (!data) return <div>Loading</div>
 
   return (
     <div className='flex flex-col'>
       <Header />
       <Hero />
-
-      {
-        data.map((user: DataType) => {
-          return <div key={user.id}>{user.name} <button onClick={() => handleDelete(user.id)}> Delete</button></div>
-        })
-      }
       <div id='about'>
         <About />
       </div>
